617b1b1c493a35bd4969f18933be17da21eb098f
JoseLuGeek <joseluisrobleslao10@gmail.com>
2025-06-20 10:39:42 +0000

    Sheldon Game

--- src/app.js
+++ src/app.js
@@ -1,3 +1,31 @@
-//write your code here
-console.log("Hello Rigo from the console!");
+const weapon = ["rock", "paper", "scissors", "lizard", "spock"]
 
+  const getRandomWeapon = () => {
+  const index = Math.floor(Math.random() * weapon.length)
+  return weapon[index]
+}
+
+const checkWeaponGuess = (userGuest) => {
+  const computerWeapon = getRandomWeapon()
+
+  console.log(`You chose: ${userGuest}`);
+  console.log(`Computer weapon: ${computerWeapon}`);
+
+  if (userGuest === computerWeapon) {
+    console.log("Take it serious! It's a draw...");
+  } else if (
+    (userGuest === "rock" && (computerWeapon === "scissors" || computerWeapon === "lizard")) ||
+    (userGuest === "paper" && (computerWeapon === "rock" || computerWeapon === "spock")) ||
+    (userGuest === "scissors" && (computerWeapon === "paper" || computerWeapon === "lizard")) ||
+    (userGuest === "lizard" && (computerWeapon === "paper" || computerWeapon === "spock")) ||
+    (userGuest === "spock" && (computerWeapon === "scissors" || computerWeapon === "rock"))
+  ) {
+    console.log("Hmm... you win. Again!");
+  } else {
+    console.log("Bazinga! You loose!");
+  }
+}
+
+const guess = prompt("Choose your weapon: rock, paper, scissors, lizard, spock").toLowerCase();
+
+checkWeaponGuess(guess)
